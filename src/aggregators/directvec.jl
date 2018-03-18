7b3b9460e32afe2da273374eddb1021fe5179903
isaacsas <isaacsas@users.noreply.github.com>
2018-03-18 11:58:45 -0400

    "updates"

--- src/aggregators/directvec.jl
+++ src/aggregators/directvec.jl
@@ -27,39 +27,36 @@ function (p::DirectVECJumpAggregation)(integrator) # affect!
 end
 
 function (p::DirectVECJumpAggregation)(dj,u,t,integrator) # initialize
-  sum_rate,next_jump = time_to_next_jump_ptr(u,integrator.p,t,p.rates,p.cur_rates)
-  p.sum_rate         = sum_rate
-  p.next_jump        = t + next_jump
-  if p.next_jump < p.end_time
+    sum_rate,next_jump = time_to_next_jump_ptr(u,integrator.p,t,p.rates,p.cur_rates)
+    p.sum_rate         = sum_rate
+    p.next_jump        = t + next_jump
+    if p.next_jump < p.end_time
     add_tstop!(integrator,p.next_jump)
-  end
-  nothing
+    end
+    nothing
 end
 
 function time_to_next_jump_ptr(u,p,t,rates,cur_rates)
-  
     @inbounds cur_rates[1] = rates[1](u,p,t)
     @inbounds sum_rate     = cur_rates[1]
     @inbounds for i in 2:length(cur_rates)
         new_rate     = rates[i](u,p,t)
         cur_rates[i] = new_rate + cur_rates[i-1]
         sum_rate    += new_rate
-    end   
-    
+    end
+
     sum_rate,randexp()/sum_rate
 end
 
 @inline function aggregate(aggregator::DirectVEC,u,p,t,end_time,constant_jumps,save_positions)
-    RateWrapper = FunctionWrappers.FunctionWrapper{typeof(t),Tuple{typeof(u), typeof(p), typeof(t)}}
-    rates = [RateWrapper(c.rate) for c in constant_jumps]
-    # AffectWrapper = FunctionWrappers.FunctionWrapper{Any,Tuple{Any}}
-    # affects! = [AffectWrapper(c.affect!) for c in constant_jumps]
-    AffectWrapper = FunctionWrappers.FunctionWrapper{Void,Tuple{Any}}
-    affects! = [AffectWrapper(x->(c.affect!(x);nothing)) for c in constant_jumps]
-    # affects! = [c.affect! for c in constant_jumps]
-    cur_rates = Vector{Float64}(length(rates))
+    RateWrapper        = FunctionWrappers.FunctionWrapper{typeof(t),Tuple{typeof(u), typeof(p), typeof(t)}}
+    rates              = [RateWrapper(c.rate) for c in constant_jumps]
+    AffectWrapper      = FunctionWrappers.FunctionWrapper{Void,Tuple{Any}}
+    affects!           = [AffectWrapper(x->(c.affect!(x);nothing)) for c in constant_jumps]
+    # affects!           = [c.affect! for c in constant_jumps]
+    cur_rates          = Vector{Float64}(length(rates))
     sum_rate,next_jump = time_to_next_jump_ptr(u,p,t,rates,cur_rates)
-    DirectVECJumpAggregation(next_jump, end_time, cur_rates, sum_rate, 
+    DirectVECJumpAggregation(next_jump, end_time, cur_rates, sum_rate,
                                         rates, affects!, save_positions)
 end
 
--- test/diffusiontest.jl
+++ test/diffusiontest.jl
@@ -1,17 +1,18 @@
-using DiffEqBase, OrdinaryDiffEq, DiffEqJump, DiffEqBiological
+using DiffEqBase, DiffEqJump, DiffEqBiological
 using DiffEqJumpExtensions
 using BenchmarkTools
 using BioSimulator
 
 # DiffEqJump methods to benchmark
 #methods = (Direct2(), DirectVEC(), DiffEqJumpExtensions.FRM(), FRMVEC())
-methods = (DirectVEC(), FRMVEC())
+#methods = (DirectVEC(), FRMVEC())
+methods = (DirectVEC(),)
 
 # number of save times to look at
 num_save_vec = 25000 #10.^(3:6)
 
-# diffusion model 
-N = 50
+# diffusion model
+N = 256
 diffnetwork = "@reaction_network difftype begin\n"
 for i in 1:(N-1)
     diffnetwork *= "\t K, X$(i) --> X$(i+1)\n"
@@ -20,7 +21,7 @@ end
 diffnetwork *= "end K"
 #println(diffnetwork)
 rs = eval( parse(diffnetwork) )
-tf = 10.
+tf = 20.
 params = (1.,)
 prob   = DiscreteProblem(10*ones(N), (0.0, tf), params)
 
@@ -30,38 +31,39 @@ for i in 1:N
     model <= Species("X$(i)")
 end
 for i in 1:(N-1)
-    model <= Reaction("diffusion$(i)a", params[1], "X$(i) --> X$(i+1)") 
-    model <= Reaction("diffusion$(i)b", params[1], "X$(i+1) --> X$(i)") 
+    model <= Reaction("diffusion$(i)a", params[1], "X$(i) --> X$(i+1)")
+    model <= Reaction("diffusion$(i)b", params[1], "X$(i+1) --> X$(i)")
 end
 
 
 # exact methods
-println("Full solution paths saving:\n")
-for method in methods
-    println("Solving with method: ", typeof(method), ", using SSAStepper")
-    jump_prob = JumpProblem(prob, method, rs)
-    @btime solve($jump_prob, SSAStepper())
-end
-println()
+# println("Full solution paths saving:\n")
+# for method in methods
+#     println("Solving with method: ", typeof(method), ", using SSAStepper")
+#     jump_prob = JumpProblem(prob, method, rs)
+#     @btime solve($jump_prob, SSAStepper())
+# end
+# println()
 
 # methods with different numbers of points
 println("Saving at fixed times:\n")
 for num_save in num_save_vec
     dtsave = tf / num_save
 
-    for method in methods    
+    for method in methods
         println("Solving with method: ", typeof(method), ", using SSAStepper and ", num_save, " points")
         jump_prob = JumpProblem(prob, method, rs, save_positions=(false,false))
-        @btime solve($jump_prob, SSAStepper(), saveat=$dtsave)                        
-    end 
+        sol = solve(jump_prob, SSAStepper(), saveat=dtsave);
+        @btime solve($jump_prob, SSAStepper(), saveat=$dtsave)
+    end
 
     # BioSimulator:
     println("BioSimulator, Direct, saving at ", num_save, " points:")
+    result = simulate(model, BioSimulator.SSA, time=tf, epochs=num_save, trials=1);
     @btime simulate($model, BioSimulator.SSA, time=$tf, epochs=$num_save, trials=1);
-    
-    println("BioSimulator, FRM, saving at ", num_save, " points:")
-    @btime simulate($model, BioSimulator.FRM, time=$tf, epochs=$num_save, trials=1);
-    
+
+    # println("BioSimulator, FRM, saving at ", num_save, " points:")
+    # @btime simulate($model, BioSimulator.FRM, time=$tf, epochs=$num_save, trials=1);
+
     println()
 end
-
