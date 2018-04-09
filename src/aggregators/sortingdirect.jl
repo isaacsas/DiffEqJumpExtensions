f4d6f75438553573826ace653db7ea5439093317
isaacsas <isaacsas@users.noreply.github.com>
2018-04-09 18:39:42 -0400

    dep graphs for mass action jumps implemented, started on sorting direct

---
+++ src/aggregators/sortingdirect.jl
@@ -0,0 +1,146 @@
+mutable struct SortingDirectJumpAggregation{T,S,F1,F2,RNG,DEPGR} <: AbstractSSAJumpAggregator
+    next_jump::Int
+    next_jump_time::T
+    end_time::T
+    cur_rates::Vector{T}
+    sum_rate::T
+    ma_jumps::S
+    rates::F1
+    affects!::F2
+    save_positions::Tuple{Bool,Bool}
+    rng::RNG
+    dep_gr::DEPGR
+    jump_search_order::Vector{Int}
+    jump_search_idx::Int
+    SortingDirectJumpAggregation{T,S,F1,F2,RNG}(nj::Int, njt::T, et::T, crs::Vector{T}, sr::T, maj::S, 
+                                                rs::F1, affs!::F2, sps::Tuple{Bool,Bool}, rng::RNG, 
+                                                dep_gr::DEPGR, jtoidx::Vector{Int}) where {T,S,F1,F2,RNG,DEPGR} = 
+      new{T,S,F1,F2,RNG,DEPGR}(nj, njt, et, crs, sr, maj, rs, affs!, sps, rng, dep_gr, jtoidx, zero(Int))
+  end
+
+function SortingDirectJumpAggregation(nj::Int, njt::T, et::T, crs::Vector{T}, sr::T, 
+                            maj::S, rs::F1, affs!::F2, sps::Tuple{Bool,Bool}, 
+                            rng::RNG; dep_graph=nothing, kwargs...) where {T,S,F1,F2,RNG} 
+    
+    # a dependency graph is needed and must be provided if there are constant rate jumps
+    if dep_graph == nothing
+        if isempty(maj.rates) || !isempty(rs)
+            error("To run a simulation with any ConstantRateJumps a dependency graph must be supplied.")
+        else
+            dg = make_dependency_graph(length(maj.scaled_rates), maj)
+        end
+    else
+        dg = dep_graph
+    end
+
+    # map jump idx to idx in cur_rates
+    jtoidx = collect(1:length(crs))
+    SortingDirectJumpAggregation{T,S,F1,F2,RNG}(nj, njt, et, crs, sr, maj, rs, affs!, sps, rng, dg, jtoidx)
+end
+
+
+  ########### The following routines should be templates for all SSAs ###########
+
+# condition for jump to occur
+@inline function (p::SortingDirectJumpAggregation)(u, t, integrator) 
+    p.next_jump_time == t
+end
+  
+# executing jump at the next jump time
+function (p::SortingDirectJumpAggregation)(integrator) 
+    execute_jumps!(p, integrator, integrator.u, integrator.p, integrator.t)
+    generate_jumps!(p, integrator, integrator.u, integrator.p, integrator.t)
+    register_next_jump_time!(integrator, p, integrator.t)
+    nothing
+end
+  
+# setting up a new simulation
+function (p::SortingDirectJumpAggregation)(dj, u, t, integrator) # initialize
+    initialize!(p, integrator, u, integrator.p, t)
+    register_next_jump_time!(integrator, p, t)
+    nothing
+end
+  
+  # creating the JumpAggregation structure (function wrapper-based constant jumps)
+function aggregate(aggregator::SortingDirect, u, p, t, end_time, constant_jumps, 
+                   ma_jumps, save_positions, rng; kwargs...)
+
+    # handle constant jumps using function wrappers
+    rates, affects! = get_jump_info_fwrappers(u, p, t, constant_jumps)
+
+    build_jump_aggregation(SortingDirectJumpAggregation, u, p, t, end_time, ma_jumps, 
+                           rates, affects!, save_positions, rng; kwargs...)
+end
+
+# set up a new simulation and calculate the first jump / jump time
+function initialize!(p::SortingDirectJumpAggregation, integrator, u, params, t)
+    fill_rates_and_sum!(p, u, params, t)
+    generate_jumps!(p, integrator, u, params, t)
+    nothing
+end
+  
+# execute one jump, changing the system state
+@inline function execute_jumps!(p::SortingDirectJumpAggregation, integrator, u, params, t)
+    num_ma_rates = length(p.ma_jumps.scaled_rates)
+    if p.next_jump <= num_ma_rates
+        @inbounds executerx!(u, p.ma_jumps.net_stoch[p.next_jump])
+    else
+        idx = p.next_jump - num_ma_rates
+        @inbounds p.affects![idx](integrator)
+    end
+    nothing
+end
+  
+# calculate the next jump / jump time
+function generate_jumps!(p::SortingDirectJumpAggregation, integrator, u, params, t)
+    @fastmath p.next_jump_time = t + calc_next_jump(p, u, params, t)
+    nothing
+end
+  
+
+######################## SSA specific helper routines ########################
+
+function fill_rates_and_sum!(p, u, params, t)
+    sum_rate = zero(typeof(p.sum_rate))
+
+    # mass action jumps
+    majumps   = p.ma_jumps
+    cur_rates = p.cur_rates
+    @inbounds for i in eachindex(majumps.scaled_rates)
+        cur_rates[i] = evalrxrate(u, majumps.scaled_rates[i], majumps.reactant_stoch[i])
+        sum_rate    += cur_rates[i]
+    end
+
+    # constant rates (IGNORED)
+    # rates = p.rates
+    # idx   = length(majumps.scaled_rates) + 1
+    # @inbounds for i in eachindex(rates)
+    #     cur_rates[idx] = rates[i](u, params, t)
+    #     sum_rate += cur_rates[idx]
+    #     idx += 1
+    # end
+
+    p.sum_rate = sum_rate
+end
+
+# searches down the rate list for the next reaction 
+@fastmath function calc_next_jump(p, u, params, t)
+    
+    # time to next jump
+    ttnj = randexp(p.rng) / p.sum_rate
+
+    # next jump type
+    numjumps = length(p.cur_rates)
+    jso      = p.jump_search_order
+    rn       = p.sum_rate * rand(p.rng)
+    for idx = 1:numjumps
+        rn = rn - cur_rates[jso[idx]]
+        if rn < zero(rn)
+            p.jump_search_idx = idx            
+            break
+        end
+    end
+    p.next_jump = jso[idx]
+
+    return ttnj
+end
