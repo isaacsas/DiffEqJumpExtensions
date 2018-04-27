ea11c88ac5203263f13e8dc7a38a799fcae39f0f
Samuel Isaacson <isaacsas@users.noreply.github.com>
2018-04-27 11:56:07 -0400

    updates

---
+++ examples/geneexpr_test_mat.jl
@@ -0,0 +1,136 @@
+using DiffEqBase, DiffEqJump
+using Base.Test
+
+using Plots; plotlyjs()
+doplot = false
+using BenchmarkTools
+dobenchmark = true
+
+dotestmean   = false
+doprintmeans = false
+
+# SSAs to test
+#SSAalgs = (SortingDirect(),Direct()) #, DirectFW(), FRM(), FRMFW())
+SSAalgs = (Direct(), SortingDirect(),)
+
+# Nsims        = 80000
+# tf           = 100000.0
+Nsims        = 100
+tf           = 10000.0
+u0           = [1,0,0,0]
+expected_avg = 5.926553750000000e+02
+reltol       = .01
+
+# average number of proteins in a simulation
+function runSSAs(jump_prob)
+    Psamp = zeros(Int, Nsims)
+    for i in 1:Nsims
+        sol = solve(jump_prob, SSAStepper())
+        Psamp[i] = sol[3,end]
+    end
+    mean(Psamp)
+end
+
+# MODEL SETUP
+
+# DNA repression model DiffEqBiological
+# using DiffEqBiological
+# rs = @reaction_network dtype begin
+#     k1, DNA --> mRNA + DNA
+#     k2, mRNA --> mRNA + P
+#     k3, mRNA --> 0
+#     k4, P --> 0
+#     k5, DNA + P --> DNAR
+#     k6, DNAR --> DNA + P
+# end k1 k2 k3 k4 k5 k6
+
+# model using mass action jumps
+# ids: DNA=1, mRNA = 2, P = 3, DNAR = 4
+reactstoch =
+[
+    [1 => 1],
+    [2 => 1],
+    [2 => 1],
+    [3 => 1],
+    [1 => 1, 3 => 1],
+    [4 => 1]
+]
+netstoch =
+[
+    [2 => 1],
+    [3 => 1],
+    [2 => -1],
+    [3 => -1],
+    [1 => -1, 3 => -1, 4 => 1],
+    [1 => 1, 3 => 1, 4 => -1]
+]
+# using StaticArrays
+# reactstoch =
+# [
+#     SVector{3}([1 => 1, 0 => 1, 0 => 1]),
+#     SVector{3}([2 => 1, 0 => 1, 0 => 1]),
+#     SVector{3}([2 => 1, 0 => 1, 0 => 1]),
+#     SVector{3}([3 => 1, 0 => 1, 0 => 1]),
+#     SVector{3}([1 => 1, 3 => 1, 0 => 1]),
+#     SVector{3}([4 => 1, 0 => 1, 0 => 1])
+# ]
+# netstoch =
+# [
+#     SVector{6}([2 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1]),
+#     SVector{6}([3 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1]),
+#     SVector{6}([2 => -1, 0 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1]),
+#     SVector{6}([3 => -1, 0 => 1, 0 => 1, 0 => 1, 0 => 1, 0 => 1]),
+#     SVector{6}([1 => -1, 3 => -1, 4 => 1, 0 => 1, 0 => 1, 0 => 1]),
+#     SVector{6}([1 => 1, 3 => 1, 4 => -1, 0 => 1, 0 => 1, 0 => 1])
+# ]
+rates = [50, 50*(20*log(2.)/120.), (log(2.)/120.), (log(2.)/600.), .025, 1.]
+#rates = [.5, (20*log(2.)/120.), (log(2.)/120.), (log(2.)/600.), .025, 1.]
+majumps = MassActionJump(rates, reactstoch, netstoch)
+
+
+# TESTING:
+prob = DiscreteProblem(u0, (0.0, tf), rates)
+
+# plotting one full trajectory
+if doplot
+    plothand = plot(reuse=false)
+    for alg in SSAalgs
+        jump_prob = JumpProblem(prob, alg, majumps, save_positions=(false,false))
+        sol = solve(jump_prob, SSAStepper(), saveat=(tf/1000.))
+        plot!(plothand, sol.t, sol[3,:], seriestype=:steppost, lab=string(alg))
+    end
+    display(plothand)
+end
+
+# test the means
+if dotestmean
+    means = zeros(Float64,length(SSAalgs))
+    for (i,alg) in enumerate(SSAalgs)
+        jump_prob = JumpProblem(prob, alg, majumps, save_positions=(false,false))
+        means[i]  = runSSAs(jump_prob)
+        relerr = abs(means[i] - expected_avg) / expected_avg
+        if doprintmeans
+            println("Mean from method: ", typeof(alg), " is = ", means[i], ", rel err = ", relerr)
+        end
+
+        if dobenchmark
+            @btime (runSSAs($jump_prob);)
+        end
+
+
+        @test abs(means[i] - expected_avg) < reltol*expected_avg
+    end
+end
+
+
+# benchmark performance
+if dobenchmark
+    # exact methods
+    for alg in SSAalgs
+        println("Solving with method: ", typeof(alg), ", using SSAStepper")
+        jump_prob = JumpProblem(prob, alg, majumps, save_positions=(false,false))
+        @btime solve($jump_prob, SSAStepper(), saveat=(tf/1000.))
+        @btime (runSSAs($jump_prob);)
+    end
+    println()
+end
---
+++ examples/twenty_gene_model.jl
@@ -0,0 +1,107 @@
+# from McCollum et al, 
+# "The sorting direct method for stochastic simulation of biochemical systems with varying reaction execution behavior"
+# Comp. Bio. and Chem., 30, pg. 39-49 (2006). 
+
+using DiffEqBase, DiffEqJump, DiffEqBiological
+using Base.Test
+
+using Plots; plotlyjs()
+doplot = false
+using BenchmarkTools
+dobenchmark = true
+
+dotestmean   = false
+doprintmeans = false
+
+# SSAs to test
+#SSAalgs = (SortingDirect(),Direct()) #, DirectFW(), FRM(), FRMFW())
+SSAalgs = (Direct(), SortingDirect(),)
+
+# Nsims        = 80000
+# tf           = 100000.0
+Nsims        = 100
+tf           = 500.0
+u0           = [1,0,0,0]
+expected_avg = 5.926553750000000e+02
+reltol       = .01
+
+# average number of proteins in a simulation
+function runSSAs(jump_prob)
+    Psamp = zeros(Int, Nsims)
+    for i in 1:Nsims
+        sol = solve(jump_prob, SSAStepper())
+        Psamp[i] = sol[3,end]
+    end
+    mean(Psamp)
+end
+
+# MODEL SETUP
+N = 20
+genenetwork = "@reaction_network gtype begin\n"
+for i in 1:N
+    genenetwork *= "\t 10.0, G$(2*i-1) --> G$(2*i-1) + M$(2*i-1)\n"
+    genenetwork *= "\t 10.0, M$(2*i-1) --> M$(2*i-1) + P$(2*i-1)\n"
+    genenetwork *= "\t 1.0,  M$(2*i-1) --> 0\n"
+    genenetwork *= "\t 1.0,  P$(2*i-1) --> 0\n"
+
+    genenetwork *= "\t 5.0, G$(2*i) --> G$(2*i) + M$(2*i)\n"
+    genenetwork *= "\t 5.0, M$(2*i) --> M$(2*i) + P$(2*i)\n"
+    genenetwork *= "\t 1.0,  M$(2*i) --> 0\n"
+    genenetwork *= "\t 1.0,  P$(2*i) --> 0\n"
+
+    genenetwork *= "\t 0.0001, G$(2*i) + P$(2*i-1) --> G$(2*i)_ind \n"
+    genenetwork *= "\t 100., G$(2*i)_ind --> G$(2*i)_ind + M$(2*i)\n"
+end
+genenetwork *= "end"
+rs = eval( parse(genenetwork) )
+u0 = zeros(Int, length(rs.syms))
+for i = 1:(2*N)
+    u0[findfirst(rs.syms, Symbol("G$(i)"))] = 1    
+end
+
+# TESTING:
+prob = DiscreteProblem(u0, (0.0, tf))
+
+# plotting one full trajectory
+if doplot
+    plothand = plot(reuse=false)
+    for alg in SSAalgs
+        jump_prob = JumpProblem(prob, alg, rs, save_positions=(false,false))
+        sol = solve(jump_prob, SSAStepper(), saveat=(tf/1000.))
+        plot!(plothand, sol.t, sol[[3,6],:]', seriestype=:steppost, lab=string(alg))
+    end
+    display(plothand)
+end
+
+# # test the means
+# if dotestmean
+#     means = zeros(Float64,length(SSAalgs))
+#     for (i,alg) in enumerate(SSAalgs)
+#         jump_prob = JumpProblem(prob, alg, majumps, save_positions=(false,false))
+#         means[i]  = runSSAs(jump_prob)
+#         relerr = abs(means[i] - expected_avg) / expected_avg
+#         if doprintmeans
+#             println("Mean from method: ", typeof(alg), " is = ", means[i], ", rel err = ", relerr)
+#         end
+
+#         if dobenchmark
+#             @btime (runSSAs($jump_prob);)
+#         end
+
+
+#         @test abs(means[i] - expected_avg) < reltol*expected_avg
+#     end
+# end
+
+
+# # benchmark performance
+if dobenchmark
+    # exact methods
+    for alg in SSAalgs
+        println("Solving with method: ", typeof(alg), ", using SSAStepper")
+        jump_prob = JumpProblem(prob, alg, rs, save_positions=(false,false))
+        @btime solve($jump_prob, SSAStepper(), saveat=(tf/1000.))
+        #@btime (runSSAs($jump_prob);)
+    end
+    println()
+end
